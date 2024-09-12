5b3fae473e90ddce6e38d05b98fb21cc65a3ef6b
momodedev <v-chengzhiz@microsoft.com>
2024-09-12 13:51:32 +0800

    Update provider.tf with backend

--- provider.tf
+++ provider.tf
@@ -1,4 +1,9 @@
 terraform {
+  backend "s3" {
+    bucket = u620"
+    key     = "/u620/terraform.tfstate"
+    region  = "us-west-2"
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
