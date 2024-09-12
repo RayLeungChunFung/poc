635af75f5318f0d5d09ad12fc85499662d71c06b
momodedev <v-chengzhiz@microsoft.com>
2024-09-12 14:15:55 +0800

    Update again again provider.tf

--- provider.tf
+++ provider.tf
@@ -1,16 +1,19 @@
 terraform {
   required_providers {
     aws = {
-      access_key = var.aws_access_key
-      secret_key = var.aws_secret_key
-}
+      source  = "hashicorp/aws"
+      version = "~> 3.72.0"
     }
   }
 
-  backend "http" {
-  }
+  backend "s3" {
+    bucket = "u620"
+    key     = "u620/terraform.tfstate"
+    region  = "us-west-2"
 }
 
 provider "aws" {
   region = "us-west-2"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
